7e386782dfa138b2042e2fa8cf188a72bde208e5
John Stratton <jstrat0407@gmail.com>
2017-09-20 20:46:15 -0400

    require router.js in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const router = require('./router');
+
 // create webserver
 const http = require('http');
 http.createServer(function (request, response) {
-  homeRoute(request, response);
+  router.home(request, response);
+  router.comicQuery(request, response);
 }).listen(3000);
 console.log('Server running at http://<your url>:3000');
