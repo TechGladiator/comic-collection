321624c0363e81403cfe377810a5db0e7cfd9983
John Stratton <jstrat0407@gmail.com>
2017-09-20 20:00:41 -0400

    handled root path
    also added console log when server is run

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // create webserver
 const http = require('http');
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end(
-    `<h1>Welcome To Comic Collection!</h1>
-     <h2>Coming Soon</h2>`
-  );
+  homeRoute(request, response);
 }).listen(3000);
+console.log('Server running at http://<your url>:3000');
+
+// handle HTTP route GET / and POST / i.e. Home
+function homeRoute (request, response) {
+  if(request.url === "/") {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(
+      `<h1>Welcome To Comic Collection!</h1>
+       <h2>Coming Soon</h2>`
+    );
+  }
+}
