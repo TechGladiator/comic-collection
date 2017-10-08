7f5b794d16941fd70262645931be51a7a02aefbe
John Stratton <jstrat0407@gmail.com>
2017-10-08 16:45:05 -0400

    respond with "Hello World!" when testing server

--- src/server.js
+++ src/server.js
@@ -2,3 +2,9 @@ const express = require('express');
 const config = require('./config');
 
 const app = express();
+
+// response when testing server
+app.use((req, res, next) => {
+  res.end("Hello World!");
+});
+
