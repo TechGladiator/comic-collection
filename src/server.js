b894826fba14ecd1ae75e9db93837d2fb0fb07bd
John Stratton <jstrat0407@gmail.com>
2017-10-09 00:34:06 -0400

    added src/public as static file path

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const config = require('./config');
 
 const app = express();
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
 
 // documentation route
 app.use('/doc', function(req, res, next) {
