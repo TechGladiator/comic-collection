faf284319ae24eeecc717d0b7868417f46c4a9fb
John Stratton <jstrat0407@gmail.com>
2017-10-22 23:48:08 -0400

    added update, delete, and read endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,24 @@ router.use('/doc', function(req, res, next) {
   res.end(`Documentation https://techgladiator.github.io/comic-collection`);
 });
 
+router.get('/file', function(req, res, next) {
+  res.end('List all files');
+});
+
+router.post('/file', function(req, res, next) {
+  res.end('Create a new file');
+});
+
+router.put('/file/:fileId', function(req, res, next) {
+  res.end(`Updating file '${req.params.fileId}'`);
+});
+
+router.delete('/file/:fileId', function(req, res, next) {
+  res.end(`Deleting file '${req.params.fileId}'`);
+});
+
+router.get('/file/:fileId', function(req, res, next) {
+  res.end(`Reading file '${req.params.fileId}'`);
+});
+
 module.exports = router;
