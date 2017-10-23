cb7b131eda508c8308c88c504348234adce601db
John Stratton <jstrat0407@gmail.com>
2017-10-22 23:31:52 -0400

    require routes and added /api route

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const config = require('./config');
+const router = require('./routes');
 
 const app = express();
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
+app.use('/api', router);
 
 app.set('views', './src/views')
 app.set('view engine', 'pug');
@@ -14,16 +16,6 @@ app.set('view engine', 'pug');
 app.get('/', function (req, res) {
   res.render('index', { title: 'Comic Collection', message: 'Hello Comic Collector!' })
 })
-
-// documentation route
-app.use('/doc', function(req, res, next) {
-  res.end(`Documentation https://techgladiator.github.io/comic-collection`);
-});
-
-// response when testing server
-app.use((req, res, next) => {
-  res.end("Hello World!");
-});
 
 // Start the server
 app.listen(config.port, function() {
