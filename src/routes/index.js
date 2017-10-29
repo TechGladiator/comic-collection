9d616e65aa1929dc329adec47ed2d840ba3ae0c7
John Stratton <jstrat0407@gmail.com>
2017-10-29 04:31:26 -0400

    corrected files template id numbering

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@
 const router = require('express').Router();
 
 const FILES = [
-  {id: '1', series: 'The Incredible Hulk', volume: '1962', issue: '#1', coverDate: 'May, 1962'},
-  {id: '2', series: 'The Incredible Hulk', volume: '1962', issue: '#2', coverDate: 'July, 1962'},
-  {id: '3', series: 'The Incredible Hulk', volume: '1962', issue: '#3', coverDate: 'August, 1962'},
-  {id: '4', series: 'The Incredible Hulk', volume: '1962', issue: '#4', coverDate: 'October, 1962'},
-  {id: '5', series: 'The Incredible Hulk', volume: '1962', issue: '#5', coverDate: 'December, 1962'},
-  {id: '6', series: 'The Incredible Hulk', volume: '1962', issue: '#6', coverDate: 'January, 1963'}
+  {id: '0', series: 'The Incredible Hulk', volume: '1962', issue: '#1', coverDate: 'May, 1962'},
+  {id: '1', series: 'The Incredible Hulk', volume: '1962', issue: '#2', coverDate: 'July, 1962'},
+  {id: '2', series: 'The Incredible Hulk', volume: '1962', issue: '#3', coverDate: 'August, 1962'},
+  {id: '3', series: 'The Incredible Hulk', volume: '1962', issue: '#4', coverDate: 'October, 1962'},
+  {id: '4', series: 'The Incredible Hulk', volume: '1962', issue: '#5', coverDate: 'December, 1962'},
+  {id: '5', series: 'The Incredible Hulk', volume: '1962', issue: '#6', coverDate: 'January, 1963'}
 ];
 
 // documentation route
@@ -20,11 +20,26 @@ router.get('/file', function(req, res, next) {
 });
 
 router.post('/file', function(req, res, next) {
-  res.end('Create a new file');
+  const newId = '' + FILES.length;
+  const data = req.body;
+  data.id = newId;
+
+  FILES.push(data);
+  res.status(201).json(data);
 });
 
 router.put('/file/:fileId', function(req, res, next) {
-  res.end(`Updating file '${req.params.fileId}'`);
+  const {fileId} = req.params;
+  const file = FILES.find(entry => entry.id === fileId);
+  if (!file) {
+    return res.status(404).end(`Could not find file '${fileId}'`);
+  }
+
+  file.series = req.body.series;
+  file.volume = req.body.volume;
+  file.issue = req.body.issue;
+  file.coverDate = req.body.coverDate;
+  res.json(file);
 });
 
 router.delete('/file/:fileId', function(req, res, next) {
