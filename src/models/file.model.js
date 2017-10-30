f56316f0002070c0ec14ca61c7255baeae3895fd
John Stratton <jstrat0407@gmail.com>
2017-10-30 06:24:13 +0000

    add seed data if no data exists

--- src/models/file.model.js
+++ src/models/file.model.js
@@ -11,3 +11,20 @@ const FileSchema = new mongoose.Schema({
 
 const File = mongoose.model('File', FileSchema);
 module.exports = File;
+
+// count documents in collection
+File.count({}, function(err, count) {
+  if (err) {
+    throw err;
+  }
+  if (count > 0) return;
+  
+  // add seed data
+  const files = require('./file.seed.json');
+  File.create(files, function(err, newFiles) {
+    if (err) {
+      throw err;
+    }
+    console.log("DB seeded")
+  });
+});
