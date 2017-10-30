b1d17c998bc0e601080894a53060b2780b4d366d
John Stratton <jstrat0407@gmail.com>
2017-10-30 05:42:31 +0000

    connect to mongo through mongoose

--- src/server.js
+++ src/server.js
@@ -2,9 +2,15 @@
 const path = require('path');
 const bodyParser = require('body-parser');
 
+// Load mongoose package
+const mongoose = require('mongoose');
+
 const express = require('express');
 const config = require('./config');
 const router = require('./routes');
+
+// Connect to MongoDB and create/use database as configured
+mongoose.connection.openUri(`mongodb://${config.db.host}/${config.db.dbName}`);
 
 const app = express();
 const publicPath = path.resolve(__dirname, '../public');
