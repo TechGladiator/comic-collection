3c3a74f27883a24f2ff832486a745d3f6687f79e
John Stratton <jstrat0407@gmail.com>
2017-10-31 04:05:25 -0400

    update delete route to mark file as deleted

--- src/routes/index.js
+++ src/routes/index.js
@@ -80,7 +80,25 @@ router.put('/file/:fileId', function(req, res, next) {
 });
 
 router.delete('/file/:fileId', function(req, res, next) {
-  res.end(`Deleting file '${req.params.fileId}'`);
+  const File = mongoose.model('File');
+  const fileId = req.params.fileId;
+
+  File.findById(fileId, function(err, file) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+    if (!file) {
+      return res.status(404).json({message: "File not found"});
+    }
+
+    file.deleted = true;
+
+    file.save(function(err, doomedFile) {
+      res.json(doomedFile);
+    })
+
+  })
 });
 
 router.get('/file/:fileId', function(req, res, next) {
