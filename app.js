6e00ec122504e8e937b485b57199a1dbd0c07ca3
John Stratton <jstrat0407@gmail.com>
2017-10-31 06:21:54 -0400

    deleted app.js and router.js
    not being used

--- app.js
+++
@@ -1,9 +0,0 @@
-const router = require('./router');
-
-// create webserver
-const http = require('http');
-http.createServer(function (request, response) {
-  router.home(request, response);
-  router.comicQuery(request, response);
-}).listen(3000);
-console.log('Server running at http://<your url>:3000');
