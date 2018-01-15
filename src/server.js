fbc9ecf1b9b1cd46e75087b221b8750f7695f6c0
John Stratton <jstrat0407@gmail.com>
2018-01-15 18:31:54 -0500

    Revert "Remove Pug and use public/index.html"
    This reverts commit 2017224fce9a8a17204e11631b6d9ae3fee0d318.

--- src/server.js
+++ src/server.js
@@ -25,6 +25,14 @@ app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use('/api', router);
 
+app.set('views', './src/views')
+app.set('view engine', 'pug');
+
+// index page route
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
 // Start the server
 app.listen(config.port, function() {
   console.log(`${config.appName} is listening on port ${config.port}`);
